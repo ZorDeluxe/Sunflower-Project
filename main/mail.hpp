b1d26122204cf2471ba17a892d73c2cd335a73a6
ZorDeluxe <zoren.delacruz27@gmail.com>
2023-11-05 22:17:58 +1300

    Mail server

--- main/mail.hpp
+++ main/mail.hpp
@@ -8,7 +8,22 @@
 */
 #pragma once
 
-class Mail {
-  
-  public:
-}
+#ifndef EmailSender_hpp
+#define EmailSender_hpp
+
+#include <Arduino.h>
+#include <ESP_Mail_Client.h>
+
+class EmailSender {
+public:
+    EmailSender(const char* ssid, const char* password, const char* smtpHost, int smtpPort, const char* authorEmail, const char* authorPassword, const char* recipientEmail);
+    void sendEmail(const char* subject, const char* message);
+
+private:
+    SMTPSession smtp;
+    void connectToWiFi(const char* ssid, const char* password);
+    void configureSMTPSession(const char* smtpHost, int smtpPort, const char* authorEmail, const char* authorPassword);
+    void setupSMTPSession();
+};
+
+#endif
